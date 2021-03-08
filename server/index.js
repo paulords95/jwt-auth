b24df3a6b17a276b2e95e291084260aab507948f
paulords95 <paulords95@gmail.com>
2021-03-07 22:13:11 -0300

    created server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+
+
+
+
+app.listen(5000, () => {
+    console.log('server running at 5000')
+})
