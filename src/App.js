7110a77989f42270cbfcea70a1b650f80b4000e3
paulords95 <paulords95@gmail.com>
2021-03-10 18:49:04 -0300

    added auth func

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React, {Fragment} from 'react'
+import React, {Fragment, useState} from 'react'
 import './App.css';
 
 
@@ -11,14 +11,22 @@ import Login from './components/Login'
 import Register from './components/Register'
 
 function App() {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+
+  const setAuth = (boolean) => {
+    setIsAuthenticated(boolean)
+  }
+
   return (
     <Fragment>
       <Router>
         <div className='container'>
           <Switch>
-            <Route exact path='/login' render={props => <Login {...props} />} />
-            <Route exact path='/register' render={props => <Register {...props} />} />
-            <Route exact path='/dashboard' render={props => <Dashboard {...props} />}/>
+            <Route exact path='/login' render={props => !isAuthenticated ? <Login {...props} setAuth={setAuth} /> : <Redirect to='/dashboard' />} />
+            <Route exact path='/register' render={props =>  !isAuthenticated ? <Register {...props} setAuth={setAuth} /> : <Redirect to='/login'/>} />
+            <Route exact path='/dashboard' render={props => isAuthenticated ? <Dashboard {...props} setAuth={setAuth} /> : <Redirect to='/login'/>}/>
         </Switch>
         </div>
         
