f6a74bc659de21dc1bcc3d6b200bc5204377496a
paulords95 <paulords95@gmail.com>
2021-03-10 18:41:36 -0300

    created pages and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+
+import React, {Fragment} from 'react'
 import './App.css';
+
+
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+
+
+import Dashboard from './components/Dashboard'
+import Login from './components/Login'
+import Register from './components/Register'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <Fragment>
+      <Router>
+        <div className='container'>
+          <Switch>
+            <Route exact path='/login' render={props => <Login {...props} />} />
+            <Route exact path='/register' render={props => <Register {...props} />} />
+            <Route exact path='/dashboard' render={props => <Dashboard {...props} />}/>
+        </Switch>
+        </div>
+        
+      </Router>
+
+   </Fragment>
   );
 }
 
