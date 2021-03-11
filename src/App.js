bc45e84aba31e344e7da302441ec7161965398d8
paulords95 <paulords95@gmail.com>
2021-03-10 22:18:02 -0300

    added / default route

--- src/App.js
+++ src/App.js
@@ -47,6 +47,7 @@ function App() {
       <Router>
         <div className='container'>
           <Switch>
+            <Route exact path='/' render={props => !isAuthenticated ? <Login {...props} setAuth={setAuth} /> : <Redirect to='/dashboard' />} />
             <Route exact path='/login' render={props => !isAuthenticated ? <Login {...props} setAuth={setAuth} /> : <Redirect to='/dashboard' />} />
             <Route exact path='/register' render={props =>  !isAuthenticated ? <Register {...props} setAuth={setAuth} /> : <Redirect to='/login'/>} />
             <Route exact path='/dashboard' render={props => isAuthenticated ? <Dashboard {...props} setAuth={setAuth} /> : <Redirect to='/login'/>}/>
